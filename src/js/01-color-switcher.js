32a9939326947f349104c3dbb1b30dc044a2c4ed
Anna Gwadera-Dec <aniagwr@gmail.com>
2024-05-07 00:25:36 +0200

    SRC

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,22 @@
+let timeout;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBackgroundColor() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  timeout = setTimeout(changeBackgroundColor, 1000);
+}
+
+document.querySelector("button[data-start]").addEventListener("click", function () {
+  this.disabled = true;
+  document.querySelector("button[data-stop]").disabled = false;
+  changeBackgroundColor();
+});
+
+document.querySelector("button[data-stop]").addEventListener("click", function () {
+  this.disabled = true;
+  document.querySelector("button[data-start]").disabled = false;
+  clearTimeout(timeout);
+});
